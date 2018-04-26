4e1d6adc033e1a7c2a5aa0c8bc7a4efd2cabe00e
addf-msi <addf400@outlook.com>
2018-04-26 09:54:55 +0800

    Add data struct

---
+++ ConvexHull/DataStruct.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvexHull
+{
+    public class Point
+    {
+        public double x;
+        public double y;
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static Vector operator -(Point A, Point B)
+        {
+            return new Vector(A.x - B.x, A.y - B.y);
+        }
+
+        public static Point operator -(Point A, Vector B)
+        {
+            return new Point(A.x - B.x, A.y - B.y);
+        }
+
+        public static Point operator +(Point A, Vector B)
+        {
+            return new Point(A.x + B.x, A.y + B.y);
+        }
+    }
+
+    public class Vector
+    {
+        public double x;
+        public double y;
+
+        public Vector(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static double Dot(Vector A, Vector B)
+        {
+            return A.x * B.x + A.y * B.y;
+        }
+
+        public static double Cross(Vector A, Vector B)
+        {
+            return A.x * B.y - A.y - B.x;
+        }
+    }
+}
---
+++ ConvexHull/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvexHull
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
